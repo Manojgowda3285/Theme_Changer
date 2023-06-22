c6fc82fdd0e9e326150c75738fa0070f60875cc6
Manoj M <120711928+Manojgowda3285@users.noreply.github.com>
2023-06-22 18:13:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function changebackgroundColor_1() {
+    var body = document.querySelector('.body');
+    body.style.backgroundColor = '#121212';
+    var button = document.getElementById("a");
+    button.style.color = '#ffffff';
+}
+
+function changebackgroundColor_2() {
+    var body = document.querySelector('.body');
+    body.style.backgroundColor = '#262a2f';
+    var button = document.getElementById("a");
+    button.style.color = '#ffffff';
+}
+function changebackgroundColor_3() {
+    var body = document.querySelector('.body');
+    body.style.backgroundColor = '#ffffff';
+    var button = document.getElementById("b");
+    button.style.color = '#121212';
+}
